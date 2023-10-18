575718d84ce38468577400ec31a0e9e760d9229a
Sundardasps <sundardasps0055@gmail.com>
2023-10-18 17:02:46 +0530

    third commit

--- sortMethods/bubbleSort.js
+++ sortMethods/bubbleSort.js
@@ -1,19 +1,19 @@
 
-// function bubblesort(array){
-//     var  len= array.length
-//     for(var i=0;i<len;i++){
-//         for(var j=0;j<len-i-1;j++){
-//             if(array[j]>array[j+1]){
-//                 var temp=array[j]
-//                 array[j]=array[j+1]
-//                 array[j+1]=temp
-//             }
-//         }
-//     }
-//     return array
-//   }
-//   var array=[2,6,1,7,3,4,9,10]
-//   console.log(bubblesort(array));
+function bubblesort(array){
+    var  len= array.length
+    for(var i=0;i<len;i++){
+        for(var j=0;j<len-i-1;j++){
+            if(array[j]>array[j+1]){
+                var temp=array[j]
+                array[j]=array[j+1]
+                array[j+1]=temp
+            }
+        }
+    }
+    return array
+  }
+  var array=[2,6,1,7,3,4,9,10]
+  console.log(bubblesort(array));
 
 
 
